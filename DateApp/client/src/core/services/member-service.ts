1dd7bf925309b6481a4048ea917719e9ebbd1d5b
Piotr Poloczek <108583263+piotrpoloczek@users.noreply.github.com>
2025-12-26 21:51:52 +0100

    Building user interface

---
+++ DateApp/client/src/core/services/member-service.ts
@@ -0,0 +1,25 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Member, Photo } from '../../types/member';
+import { AccountService } from './account-service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MemberService {
+  private http = inject(HttpClient);
+  private baseUrl = environment.apiUrl;
+
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'members');
+  }
+
+  getMember(id: string){  
+    return this.http.get<Member>(this.baseUrl + 'members/' + id);
+  }
+
+  getMemberPhotos(id: string){
+    return this.http.get<Photo[]>(this.baseUrl + 'members/' + id + '/photos');
+  }
+}
--- DateApp/client/src/types/user.ts
+++ DateApp/client/src/types/user.ts
@@ -3,7 +3,7 @@ export type User = {
     displayName: string;
     email: string;
     token: string;
-    iamgeUrl?: string;
+    imageUrl?: string;
 }
 
 
